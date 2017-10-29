641fded184a682aac0a910d2219f0ea384e80b9d
Jamal <Jamal@DESKTOP-00AKE97>
2017-10-29 21:07:24 +0500

    Add project files.

---
+++ ImageManipulation/ImageManipulation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageManipulation
+{
+    public class ImageManipulation
+    {
+        private ImageManipulation(){}
+        private static ImageManipulation _instanceImageManip= null;
+        public static ImageManipulation Instance => _instanceImageManip ?? (_instanceImageManip = new ImageManipulation());
+
+
+        public Image ResizeImage(string path, int width, int height)
+        {
+            Image image = Image.FromFile(path);
+            Bitmap bmp = new Bitmap(width,height);
+            using (var graphic = Graphics.FromImage(bmp))
+            {
+                graphic.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphic.SmoothingMode = SmoothingMode.HighQuality;
+                graphic.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                graphic.CompositingQuality = CompositingQuality.HighQuality;
+                graphic.DrawImage(image, 0, 0, width, height);
+            }
+            return bmp;
+        }
+
+        public void SaveImage(Image image, string path,ImageFormat imageFormat)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (FileStream fs = new FileStream(path,FileMode.Create,FileAccess.Write))
+                {
+                    image.Save(memoryStream, imageFormat);
+                    byte[] bytes = memoryStream.ToArray();
+                    fs.Write(bytes,0,bytes.Length);
+                }
+            }
+        }
+    }
+}
---
+++ ImageManipulation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Image image = ImageManipulation.Instance.ResizeImage("C:\\Users\\Jamal\\Desktop\\images\\test.jpg",199,199);
+            ImageManipulation.Instance.SaveImage(image,"C:\\Users\\Jamal\\Desktop\\images\\resized.jpg",ImageFormat.Jpeg);
+ 
+            Console.WriteLine("Images are resized");
+            Console.ReadLine();
+        }
+    }
+}
